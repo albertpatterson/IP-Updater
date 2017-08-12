034f9aa559ba5c96e98e735fb368c7dfa382527e
Albert Patterson <apatterson@alumni.cmu.edu>
2017-08-12 14:02:18 -0400

    updating to read from file and compare ip to current ip

--- ipUpdater.js
+++ ipUpdater.js
@@ -1,34 +1,78 @@
 const cp = require('child_process');
 const fs = require("fs");
 
+const ipRegExp = /\/\/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/;
 const iplog = "./iplog.txt";
 
-let fetchIPProm = new Promise((res, rej)=>{
-    let ipFetch = cp.execFile("dig",["+short", "myip.opendns.com", "@resolver1.opendns.com"])
-    ipFetch.stdout.on("data", res);
-    ipFetch.stderr.on("data", rej);
-    ipFetch.on("close", c=>console.log('dig done, code: ', c))
-});
+// compare the ip in the file to the current one and update the file if ips are different
+compareAndUpdateFile()
+// push the updated file to github if it was changed
+.then(updated => updated && pushUpdateToGitHub())
+.then(()=>console.log("operation complete"))
+.catch(console.log)
 
-let readFileProm = new Promise((res, rej)=>{
-    fs.readFile(iplog, 'utf8', (err, conts)=>{
-        err ? rej(err) : res(conts);
-    });
-});
 
-Promise.all([fetchIPProm, readFileProm])
-.then(ipAndoldConts=>{
-    let [ip, oldConts] = ipAndoldConts;
-    console.log("update IP in log to: ", ip);
-    console.log("oldConts: ", oldConts);
-    return new Promise((res, rej)=>{
-        let newCont = oldConts + "\n" + new Date().toUTCString() + "\n" + ip;
-        fs.writeFile(iplog, newCont, err=>{err ? rej(err): res() })
+/**
+ * compare the current IP to the IP in the file and update the file if needed
+ * 
+ * @returns {Promise<Boolean>} promise resolved with a boolean indicating if the file was updated
+ */
+function compareAndUpdateFile(){
+
+    // fetch the current IP
+    let fetchIPProm = new Promise((res, rej)=>{
+        // let ipFetch = cp.execFile("dig",["+short", "myip.opendns.com", "@resolver1.opendns.com"])
+        // ipFetch.stdout.on("data", res);
+        // ipFetch.stderr.on("data", rej);
+        // ipFetch.on("close", c=>console.log('dig done, code: ', c))
+        res("173.48.210.203");
     });
-})
-.then(()=>{
-    console.log("git add");
+    
+    // get the text from the file containing the IP
+    let readFileProm = new Promise((res, rej)=>{
+        fs.readFile(iplog, 'utf8', (err, conts)=>{
+            err ? rej(err) : res(conts);
+        });
+    });
+
+   return Promise.all([fetchIPProm, readFileProm])
+    .then(ipAndOldConts=>{
+        // compare the current IP to that in the file and update the file contents if needed
+
+        let [ip, oldConts] = ipAndOldConts;
+
+        let ipMatch = oldConts.match(ipRegExp);
+        if(!ipMatch){
+            throw new Error("No IP found in file");
+        }
+        let ipInFile = ipMatch[0].slice(2);
+        
+        return new Promise((res, rej)=>{
+            if(ipInFile !== ip){
+                // update the file contents if the ips do not match
+                let newConts = oldConts.replace(ipRegExp, '//'+ip);
+                fs.writeFile(iplog, newConts, (err, data)=>{
+                    err ? rej(err) : res(true);
+                })
+            }else{
+                // resolve immediately if no update is required
+                res(false);
+            }
+        })
+    })
+}
+
+
+/**
+ * push the changes to the file to gitHub if needed
+ * 
+ * @returns 
+ */
+function pushUpdateToGitHub(){
+    
     return new Promise((res, rej)=>{
+        // add all changes to github
+
         let add = cp.execFile("git",["add", "."])
         add.stdout.on("data", d=>console.log('add data: ', d))
         add.stderr.on("data", rej);
@@ -37,34 +81,32 @@ Promise.all([fetchIPProm, readFileProm])
             if(c===0) res();
         })
     })
-})
-.then(()=>{
-    console.log("git commit");
-    return new Promise((res, rej)=>{
-        let commit = cp.execFile("git",["commit", "-m","\"updating IP\""])
-        commit.stdout.on("data", d=>console.log('commit data: ', d))
-        commit.stderr.on("data", rej);
-        commit.on("close", c=>{
-            console.log('git commit done, code: ', c);
-            if(c===0) res();
+    .then(()=>{
+        // commit all changes to github
+
+        console.log("git commit");
+        return new Promise((res, rej)=>{
+            let commit = cp.execFile("git",["commit", "-m","\"updating IP\""])
+            commit.stdout.on("data", d=>console.log('commit data: ', d))
+            commit.stderr.on("data", rej);
+            commit.on("close", c=>{
+                console.log('git commit done, code: ', c);
+                if(c===0) res();
+            })
         })
     })
-})
-.then(()=>{
-    console.log("git push");
-    return new Promise((res, rej)=>{
-        let push = cp.execFile("git",["push"])
-        push.stdout.on("data", d=>console.log('commit data: ', d));
-        push.stderr.on("data", rej);
-        push.on("close", c=>{
-            console.log('git commit done, code: ', c);
-            if(c===0) res();
+    .then(()=>{
+        // push all changes to github
+
+        console.log("git push");
+        return new Promise((res, rej)=>{
+            let push = cp.execFile("git",["push"])
+            push.stdout.on("data", d=>console.log('commit data: ', d));
+            push.stderr.on("data", rej);
+            push.on("close", c=>{
+                console.log('git commit done, code: ', c);
+                if(c===0) res();
+            })
         })
-    })
-})
-.then(status=>{
-    console.log("status="+status);
-})
-.catch((err)=>{
-    console.log("err="+err);
-})
+    });
+}
