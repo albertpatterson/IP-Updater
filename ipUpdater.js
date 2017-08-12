f3371dc93b1b164cc1d6a8dd75298ff98b6c5767
Albert Patterson <apatterson@alumni.cmu.edu>
2017-08-12 13:04:51 -0400

    "updating IP"

--- ipUpdater.js
+++ ipUpdater.js
@@ -3,25 +3,63 @@ const fs = require("fs");
 
 const iplog = "./iplog.txt";
 
-new Promise((res, rej)=>{
+let fetchIPProm = new Promise((res, rej)=>{
     let ipFetch = cp.execFile("dig",["+short", "myip.opendns.com", "@resolver1.opendns.com"])
     ipFetch.stdout.on("data", res);
     ipFetch.stderr.on("data", rej);
     ipFetch.on("close", c=>console.log('dig done, code: ', c))
-})
-.then(ip=>{
+});
+
+let readFileProm = new Promise((res, rej)=>{
+    fs.readFile(iplog, 'utf8', (err, conts)=>{
+        err ? rej(err) : res(conts);
+    });
+});
+
+Promise.all([fetchIPProm, readFileProm])
+.then(ipAndoldConts=>{
+    let [ip, oldConts] = ipAndoldConts;
     console.log("update IP in log to: ", ip);
+    console.log("oldConts: ", oldConts);
     return new Promise((res, rej)=>{
-        fs.writeFile(iplog, ip, err=>{err ? rej(err): res() })
+        let newCont = oldConts + "\n" + new Date().toUTCString() + "\n" + ip;
+        fs.writeFile(iplog, newCont, err=>{err ? rej(err): res() })
     });
 })
 .then(()=>{
-    console.log("git status");
+    console.log("git add");
     return new Promise((res, rej)=>{
-        let statFetch = cp.execFile("git",["status"])
-        statFetch.stdout.on("data", res);
-        statFetch.stderr.on("data", rej);
-        statFetch.on("close", c=>console.log('git status done, code: ', c))
+        let add = cp.execFile("git",["add", "."])
+        add.stdout.on("data", d=>console.log('add data: ', d))
+        add.stderr.on("data", rej);
+        add.on("close", c=>{
+            console.log('git add done, code: ', c);
+            if(c===0) res();
+        })
+    })
+})
+.then(()=>{
+    console.log("git commit");
+    return new Promise((res, rej)=>{
+        let commit = cp.execFile("git",["commit", "-m","\"updating IP\""])
+        commit.stdout.on("data", d=>console.log('commit data: ', d))
+        commit.stderr.on("data", rej);
+        commit.on("close", c=>{
+            console.log('git commit done, code: ', c);
+            if(c===0) res();
+        })
+    })
+})
+.then(()=>{
+    console.log("git push");
+    return new Promise((res, rej)=>{
+        let push = cp.execFile("git",["push"])
+        push.stdout.on("data", d=>console.log('commit data: ', d));
+        push.stderr.on("data", rej);
+        push.on("close", c=>{
+            console.log('git commit done, code: ', c);
+            if(c===0) res();
+        })
     })
 })
 .then(status=>{
