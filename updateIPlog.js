50b4f689e236aee3e37fda96d82400f7b2a63ca2
Albert Patterson <apatterson@alumni.cmu.edu>
2017-08-19 10:44:22 -0400

    testing update

--- updateIPlog.js
+++ updateIPlog.js
@@ -2,7 +2,9 @@ const ipUpdater = require('./ipUpdater');
 
 const fileToUpdate = './iplog.txt';
 
-setInterval(function(){
-    console.log('Checking IP @ ' + new Date().toUTCString());
-    ipUpdater.updateIpOnGitHub(fileToUpdate);
-}, 1*60*60e3);
+ipUpdater.updateIpOnGitHub(fileToUpdate);
+
+// setInterval(function(){
+//     console.log('Checking IP @ ' + new Date().toUTCString());
+//     ipUpdater.updateIpOnGitHub(fileToUpdate);
+// }, 1*60*60e3);
